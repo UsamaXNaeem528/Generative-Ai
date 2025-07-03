f096f483bcf6a135b6b10cfce9a016998171a48d
Usama Naeem <UsamaXNaeem528>
2025-07-03 09:21:52 +0500

    ditionaries python

---
+++ Week1_ BeginnersPython/04_DataStructures/3_dictionaries.py
@@ -0,0 +1,18 @@
+########################################################################################
+############################* DICTIONARIES IN PYTHON *##################################
+########################################################################################
+'''
+Dictionaries are used to store data values in key:value pairs.
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+A dictionary is a collection which is ordered*, changeable and do not allow duplicates.
+Changeable
+Dictionaries are changeable, meaning that we can change, add or remove items after the dictionary has been created.
+Dictionaries cannot have two items with the same key.
+'''
+
+
+
