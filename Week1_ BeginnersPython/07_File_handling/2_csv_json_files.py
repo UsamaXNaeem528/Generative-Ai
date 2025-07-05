aff2539d564245ceaa2b8503b676613682ca2977
Usama Naeem <UsamaXNaeem528>
2025-07-06 00:57:51 +0500

    push

--- Week1_ BeginnersPython/07_File_handling/2_csv_json_files.py
+++ Week1_ BeginnersPython/07_File_handling/2_csv_json_files.py
@@ -1,34 +1,34 @@
-# # '''7. ✅Working with CSV Files'''
-# # '''Reading CSV Files'''
-# import csv
-# with open(r'Week1_ BeginnersPython\07_File_handling\data.csv','r') as file:
-#     reader = csv.reader(file)
-#     for row in reader:
-#         print(row)
+# '''7. ✅Working with CSV Files'''
+# '''Reading CSV Files'''
+import csv
+with open(r'Week1_ BeginnersPython\07_File_handling\data.csv','r') as file:
+    reader = csv.reader(file)
+    for row in reader:
+        print(row)
         
-# '''Writing to CSV Files'''
-# data = [['Name', 'Age'], ['Alice', 25], ['Bob', 30]]
-# with open(r'Week1_ BeginnersPython\07_File_handling\data.csv', 'w') as file:
-#     writer = csv.writer(file)
-#     writer.writerow(data)
+'''Writing to CSV Files'''
+data = [['Name', 'Age'], ['Alice', 25], ['Bob', 30]]
+with open(r'Week1_ BeginnersPython\07_File_handling\data.csv', 'w') as file:
+    writer = csv.writer(file)
+    writer.writerow(data)
     
-# ''' 7.✅Working with CSV Files'''
+''' 7.✅Working with CSV Files'''
 import json
-# '''Reading a json'''
-# with open(r'Week1_ BeginnersPython\07_File_handling\data.json','r') as file:
-#     read_json = json.load(file)
+'''Reading a json'''
+with open(r'Week1_ BeginnersPython\07_File_handling\data.json','r') as file:
+    read_json = json.load(file)
     
-# print(read_json["students"])
+print(read_json["students"])
 
-# '''Wrting to a json'''
-# data = {
-#     'students' : [
-#         {"name": "Alice", "age": 21, "grade": "A"},
-#         {"name": "Bob", "age": 20, "grade": "B"}
-#     ]
-# }
-# with open(r'Week1_ BeginnersPython\07_File_handling\data.json','w') as file:
-#     writer = json.dump(data, file, indent=4)
+'''Wrting to a json'''
+data = {
+    'students' : [
+        {"name": "Alice", "age": 21, "grade": "A"},
+        {"name": "Bob", "age": 20, "grade": "B"}
+    ]
+}
+with open(r'Week1_ BeginnersPython\07_File_handling\data.json','w') as file:
+    writer = json.dump(data, file, indent=4)
     
 '''appending to the json'''
 #step1 : read json 
