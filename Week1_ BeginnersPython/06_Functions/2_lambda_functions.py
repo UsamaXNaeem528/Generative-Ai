a02ea4229b09a5d4c7055c08f6ff288bd006c1c7
Usama Naeem <UsamaXNaeem528>
2025-07-05 11:28:12 +0500

    push

--- Week1_ BeginnersPython/06_Functions/2_lambda_functions.py
+++ Week1_ BeginnersPython/06_Functions/2_lambda_functions.py
@@ -29,7 +29,7 @@ print(square)
 
 natural_nums = (1,2,3,4,5,6,7,8,10,11,12,13,14,15)
 odd_nums = tuple(filter(lambda x : x % 2 != 0, natural_nums))
-print(odd_nums)
+print(odd_nums)   #* (1, 3, 5, 7, 11, 13, 15)
 
 '''
 🆚 Regular Function vs Lambda
