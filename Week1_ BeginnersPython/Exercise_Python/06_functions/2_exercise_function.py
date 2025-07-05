23a5be7956cde7c05920c2e9dd12166978ffc630
Usama Naeem <UsamaXNaeem528>
2025-07-06 00:55:44 +0500

    push

--- Week1_ BeginnersPython/Exercise_Python/06_functions/2_exercise_function.py
+++ Week1_ BeginnersPython/Exercise_Python/06_functions/2_exercise_function.py
@@ -13,12 +13,13 @@ if input is 4 then it should print
 Basically number of lines it prints is equal to that number. (Hint: you need to use two for loops)
 '''
 
-def print_pattern(rows):    
-    rows += 1
-    for i in range(rows):    # Outer loop for rows
-        for j in range(i):   # Inner loop for columns
-            print('*',end='')   # Print star without newline
-        print()                 # Move to next line after inner loop
+def print_pattern(n):
+    for i in range(n):
+        i += 1
+        s=''
+        for j in range(i):
+            s = s + '*'
+        print(s)
         
 print_pattern(4)
 
