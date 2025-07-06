0d95f5530a7fa22c268252b1ce16da85d5f9ebc9
Usama Naeem <UsamaXNaeem528>
2025-07-07 03:45:41 +0500

    push

--- Week1_ BeginnersPython/Exercise_Python/07_fileHandling/2_file_handling_exercise.py
+++ Week1_ BeginnersPython/Exercise_Python/07_fileHandling/2_file_handling_exercise.py
@@ -19,6 +19,22 @@ with open(r'Week1_ BeginnersPython\Exercise_Python\07_fileHandling\new_stocks.cs
     writer.writeheader()
     writer.writerows(stocks)
             
+            
+        
+##Optimzed Version
+# with open("stocks.csv", "r") as f, open("output.csv", "w") as out:
+#     out.write("Company Name,PE Ratio, PB Ratio\n")
+#     next(f)  # This will skip first line in the file which is a header
+#     for line in f:
+#         tokens = line.split(",")
+#         stock = tokens[0]
+#         price = float(tokens[1])
+#         eps = float(tokens[2])
+#         book = float(tokens[3])
+#         pe = round(price / eps, 2)
+#         pb = round(price / book, 2)
+#         out.write(f"{stock},{pe},{pb}\n")
+
 
         
         
