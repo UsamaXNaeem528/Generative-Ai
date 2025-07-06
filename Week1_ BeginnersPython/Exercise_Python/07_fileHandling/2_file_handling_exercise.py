768f1cddaf36f565e8d27a84d542ce4bc91ec0a1
Usama Naeem <UsamaXNaeem528>
2025-07-07 03:35:50 +0500

    push

--- Week1_ BeginnersPython/Exercise_Python/07_fileHandling/2_file_handling_exercise.py
+++ Week1_ BeginnersPython/Exercise_Python/07_fileHandling/2_file_handling_exercise.py
@@ -1,29 +1,7 @@
-# stocks = []
-# import csv
-# with open(r'Week1_ BeginnersPython\Exercise_Python\07_fileHandling\stocks.csv', 'r') as file:
-#     reader = csv.DictReader(file)
-#     for row in reader:
-#         company = row['Company Name']
-#         price = int(row['Price'])
-#         earnings_per_share = int(row['Earnings Per Share'])
-#         book_value = int(row['Book Value'])
-#         pe_ratio = round(price / earnings_per_share,2)
-#         price_to_book_ratio = round(price / book_value,2)
-#         stocks.append({'Company Name': company, 
-#                        'PE Ratio': pe_ratio,
-#                        'PB Ratio' : price_to_book_ratio})
-
-# with open(r'Week1_ BeginnersPython\Exercise_Python\07_fileHandling\new_stocks.csv', 'w',newline='') as file:
-#     fieldnames = ['Company Name','PE Ratio','PB Ratio']
-#     writer = csv.DictWriter(file, fieldnames=fieldnames)
-#     writer.writeheader()
-#     writer.writerows(stocks)
-            
-
-stocks = {}
+stocks = []
 import csv
 with open(r'Week1_ BeginnersPython\Exercise_Python\07_fileHandling\stocks.csv', 'r') as file:
-    reader = file.readline()
+    reader = csv.DictReader(file)
     for row in reader:
         company = row['Company Name']
         price = int(row['Price'])
@@ -31,7 +9,7 @@ with open(r'Week1_ BeginnersPython\Exercise_Python\07_fileHandling\stocks.csv', 
         book_value = int(row['Book Value'])
         pe_ratio = round(price / earnings_per_share,2)
         price_to_book_ratio = round(price / book_value,2)
-        stocks.update({'Company Name': company, 
+        stocks.append({'Company Name': company, 
                        'PE Ratio': pe_ratio,
                        'PB Ratio' : price_to_book_ratio})
 
@@ -46,7 +24,5 @@ with open(r'Week1_ BeginnersPython\Exercise_Python\07_fileHandling\new_stocks.cs
         
         
         
-        
-    
         
     
