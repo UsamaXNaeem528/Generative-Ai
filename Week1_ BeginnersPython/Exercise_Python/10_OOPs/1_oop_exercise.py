37b0e808c6e4de53b06c46ea9961005187f7178b
Usama Naeem <UsamaXNaeem528>
2025-07-09 00:21:32 +0500

    push

---
+++ Week1_ BeginnersPython/Exercise_Python/10_OOPs/1_oop_exercise.py
@@ -0,0 +1,27 @@
+class employee:
+    def __init__(self, id, name):
+        self.id = id
+        self.name = name
+    
+    def display(self):
+        print(f'ID : {self.id}\nName : {self.name} ')
+
+#Creating a emp instance of Employee class
+emp1 = employee(22, 'Usama')
+emp1.display()
+
+#deleting the property of object
+del emp1.id
+
+try:
+    print(f'Employe Id : {emp1.id}')
+except NameError:
+    print(f"{emp1.id} attribute not exist")
+
+#deleting whole object
+del emp1
+try:
+    emp1.display()
+except NameError:
+    print(f'emp1 is note defined')
+    
